0019301719d8a0196256efbfb639044a2ebb4a2d
sullenman <sullenman@list.ru>
2026-03-22 15:46:10 +0300

    Комменты добавл

--- EventManagement/Common/Exceptions/Middleware/GlobalExceptionHandlingMiddlewareExtensions.cs
+++ EventManagement/Common/Exceptions/Middleware/GlobalExceptionHandlingMiddlewareExtensions.cs
@@ -2,8 +2,16 @@
 
 namespace EventManagement.Common.Exceptions.Middleware;
 
+/// <summary>
+/// Расширение для добавления промежуточного по в конвеер
+/// </summary>
 public static class GlobalExceptionHandlingMiddlewareExtensions
 {
+    /// <summary>
+    /// добавить глобальный обработчик исключений в конвеер
+    /// </summary>
+    /// <param name="builder">экземпляр IapplicationBuilder</param>
+    /// <returns>экземпляр IapplicationBuilder</returns>
     public static IApplicationBuilder UseGlobalExceptionHandling(this IApplicationBuilder builder)
     {
         return builder.UseMiddleware<GlobalExceptionHandlingMiddleware>();
