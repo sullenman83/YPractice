8324d01f90e14ce5c8f86f350dae57ac938d5474
sullenman <sullenman@list.ru>
2026-06-20 19:40:05 +0300

    Дописал readme написал тесты для constrains

--- Tests/EventApi.IntegrationTest/UserRepositoryTest.cs
+++ Tests/EventApi.IntegrationTest/UserRepositoryTest.cs
@@ -1,7 +1,9 @@
-﻿using EventManagement.Common;
+﻿using EventManagement.Application.Common.Exceptions;
+using EventManagement.Common;
 using EventManagement.Domain.Models;
 using EventManagement.Infrastructure.Services.UserServices;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 
@@ -48,6 +50,71 @@ public class UserRepositoryTest(DatabaseFixture fixture) : IClassFixture<Databas
 
         // Assert
         res.Should().BeEquivalentTo(user);
+    }
+
+    [Fact]
+    public async Task AddUser_NotUniqueLogin_ThrowsDbOperationException()
+    {        
+        // Arrange
+        await using var context = _fixture.Context;
+        var user = TestData.GetTestUser();
+        await context.Users.AddAsync(user);
+        await context.SaveChangesAsync();
+
+        // Act
+        var rep = new UserRepository(_fixture.Context, _logger);
+        Func<Task> act = async () => await rep.AddUserAsync(user, CancellationToken.None);
+
+        // Assert
+        await act.Should().ThrowAsync<DbOperationException>();
+    }
+
+    [Fact]
+    public async Task AddUser_LoginLEssThreeSymbols_ThrowsDbUpdateException()
+    {
+        // Arrange
+        await using var context = _fixture.Context;
+        var user = new User("Us", "password", UserRole.User);                
+        await context.Users.AddAsync(user);
+        
+
+        // Act        
+        Func<Task> act = async () => await context.SaveChangesAsync();
+
+        // Assert
+        await act.Should().ThrowAsync<DbUpdateException>();
+    }
+
+    [Fact]
+    public async Task AddUser_EmptyPassword_ThrowsDbUpdateException()
+    {
+        // Arrange
+        await using var context = _fixture.Context;
+        var user = new User("User", "", UserRole.User);
+        await context.Users.AddAsync(user);
+
+
+        // Act        
+        Func<Task> act = async () => await context.SaveChangesAsync();
+
+        // Assert
+        await act.Should().ThrowAsync<DbUpdateException>();
+    }
+
+    [Fact]
+    public async Task AddUser_IncorrectRole_ThrowsDbUpdateException()
+    {
+        // Arrange
+        await using var context = _fixture.Context;
+        var user = new User("User", "password", (UserRole)10);
+        await context.Users.AddAsync(user);
+
+
+        // Act        
+        Func<Task> act = async () => await context.SaveChangesAsync();
+
+        // Assert
+        await act.Should().ThrowAsync<DbUpdateException>();
     }
 
 
