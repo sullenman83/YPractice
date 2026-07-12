55b35d4e0bb41e0d31d13a2d3d58ccd963e0f526
sullenman <sullenman@list.ru>
2026-07-12 17:51:07 +0300

    Поправил тесты для BookingHandlerService

--- Tests/UnitTest/Bookings.Test/BookingTest.cs
+++ Tests/UnitTest/Bookings.Test/BookingTest.cs
@@ -2,10 +2,6 @@
 using DateTimeManager.Abstractions;
 using FluentAssertions;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace UnitTest;
 
