672209ca74047b48baa6578fc4fb976be2028ed3
sullenman <sullenman@list.ru>
2026-07-13 23:07:18 +0300

    добавил fixture для kafka

---
+++ Bookings/Tests/IntegrationTests/KafkaFixture.cs
@@ -0,0 +1,27 @@
+﻿using Testcontainers.Kafka;
+
+namespace Bookings.IntegrationTests;
+
+internal class KafkaFixture : IAsyncLifetime
+{
+    private readonly KafkaContainer _kafkaContainer;
+
+    public KafkaFixture()
+    {
+        _kafkaContainer = new KafkaBuilder("confluentinc/cp-kafka:7.6.0")            
+            .Build();
+    }
+
+    public string BootstrapServers => _kafkaContainer.GetBootstrapAddress();
+
+    public async Task DisposeAsync()
+    {
+        await _kafkaContainer.StopAsync().ConfigureAwait(false);
+        await _kafkaContainer.DisposeAsync().ConfigureAwait(false);
+    }
+
+    public Task InitializeAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
