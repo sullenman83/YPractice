750c70d61e58b2bf34d21eb09790e4c512e50923
sullenman <sullenman@list.ru>
2026-07-18 17:52:44 +0300

    поправил конфигурацию Docker-compose для Redis Добавил фейковый кеш еслли нет настроек для реалного кеша

--- Events/Events.Infrastructure/Extensions/InfrastructureDIExt.cs
+++ Events/Events.Infrastructure/Extensions/InfrastructureDIExt.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Npgsql;
 using StackExchange.Redis;
+using System.ComponentModel;
 using TransactionManager.Abstractions;
 using TransactionManager.Core;
 
@@ -23,7 +24,7 @@ namespace Events.Infrastructure.Extensions;
 /// <summary>
 /// Расширение для добавления сервисов инфраструктуры
 /// </summary>
-public static  class InfrastructureDIExt
+public static class InfrastructureDIExt
 {
     /// <summary>
     /// Добавить сервисмы
@@ -35,13 +36,15 @@ public static  class InfrastructureDIExt
     /// <exception cref="InvalidOperationException"></exception>
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration, IHostEnvironment env)
     {
-        var baseConnectionString = configuration.GetConnectionString("DefaultConnection")
-            ?? throw new InvalidOperationException("Не задана строка подключения к базе даных");
+        var loggerFactory = LoggerFactory.Create(logging => logging.AddConfiguration(configuration));
+        var logger = loggerFactory.CreateLogger<IServiceCollection>();
 
+        var baseConnectionString = configuration.GetConnectionString("DefaultConnection")
+            ?? throw new InvalidOperationException("Не задана строка подключения к базе даных");        
         services.Configure<BookingConfirmedConsumerSettings>(configuration.GetSection(nameof(BookingConfirmedConsumerSettings)));
         services.Configure<BookingCancelledConsumerSettings>(configuration.GetSection(nameof(BookingCancelledConsumerSettings)));
-        var redisSettings = configuration.GetSection(nameof(RedisSettings)).Get<RedisSettings>() ?? throw new InvalidOperationException("Не заданы настройки redis");
-        var redisPassword = Environment.GetEnvironmentVariable("REDIS_PASSWORD") ?? throw new InvalidOperationException("Не задана переменная окружения с паролем Redis");
+        var redisSettings = configuration.GetSection(nameof(RedisSettings)).Get<RedisSettings>();
+        var redisPassword = Environment.GetEnvironmentVariable("REDIS_PASSWORD");        
         var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? throw new InvalidOperationException("Не задана переменная окружения с паролем Postgres");
         var connectionString = new NpgsqlConnectionStringBuilder(baseConnectionString)
         {
@@ -75,33 +78,41 @@ public static  class InfrastructureDIExt
         services.AddScoped<IInboxMessageRepository, InboxMessageRepository>();
         services.AddScoped<ITransactionService, TransactionService<AppDbContext>>();
 
-        services.AddSingleton<IConnectionMultiplexer>(f =>
+        if (redisPassword == null || redisSettings == null)
         {
-            var logger = f.GetRequiredService<ILogger<ConnectionMultiplexer>>();
-            var endPoint = redisSettings.EndPoints.Split(",", StringSplitOptions.RemoveEmptyEntries);
-            var cfg = new ConfigurationOptions()
+            logger.LogInformation("Не заданы настройки Redis");            
+            services.AddSingleton<ICacheService, FakeCacheService>();
+        }        
+        else
+        {
+            services.AddSingleton<IConnectionMultiplexer>(f =>
             {
-                ConnectTimeout = redisSettings.ConnectTimeout,
-                SyncTimeout = redisSettings.SyncTimeout,
-                AsyncTimeout = redisSettings.AsyncTimeout,
-                AbortOnConnectFail = redisSettings.AbortOnConnectFail,
-                Password = redisPassword
-            };
+                var logger = f.GetRequiredService<ILogger<ConnectionMultiplexer>>();
+                var endPoint = redisSettings.EndPoints.Split(",", StringSplitOptions.RemoveEmptyEntries);
+                var cfg = new ConfigurationOptions()
+                {
+                    ConnectTimeout = redisSettings.ConnectTimeout,
+                    SyncTimeout = redisSettings.SyncTimeout,
+                    AsyncTimeout = redisSettings.AsyncTimeout,
+                    AbortOnConnectFail = redisSettings.AbortOnConnectFail,
+                    Password = redisPassword
+                };
 
-            foreach (var e in endPoint)
-            {
-                cfg.EndPoints.Add(e);
-            }           
-            
-            var multiplexer = ConnectionMultiplexer.Connect(cfg);
-            if (multiplexer.IsConnected == false)
-                logger.LogWarning("Ошибка при создании соединения к Redis");
+                foreach (var e in endPoint)
+                {
+                    cfg.EndPoints.Add(e);
+                }
 
-            multiplexer.ConnectionFailed += (sender, e) => logger.LogWarning(e.Exception, "Потеря соединения с Redis.");
-            multiplexer.ConnectionRestored += (sender, e) => logger.LogInformation("Соединение с Redis успешно установлено/восстановлено.");
-                
-            return multiplexer;
-        });
+                var multiplexer = ConnectionMultiplexer.Connect(cfg);
+                if (multiplexer.IsConnected == false)
+                    logger.LogWarning("Ошибка при создании соединения к Redis");
+
+                multiplexer.ConnectionFailed += (sender, e) => logger.LogWarning(e.Exception, "Потеря соединения с Redis.");
+                multiplexer.ConnectionRestored += (sender, e) => logger.LogInformation("Соединение с Redis успешно установлено/восстановлено.");
+
+                return multiplexer;
+            });
+        }
 
         services.AddScoped<ICacheService, CacheService>();
         
---
+++ Events/Events.Infrastructure/Services/FakeCacheService.cs
@@ -0,0 +1,28 @@
+﻿
+using Events.Application.Interfaces;
+
+namespace Events.Infrastructure.Services;
+
+/// <summary>
+/// Фековый кеш, если не передан пароль к redis используеся заглушка
+/// </summary>
+public class FakeCacheService : ICacheService
+{
+    ///<inheritdoc/>
+    public Task<bool> DeleteAsync(string key)
+    {
+        return Task.FromResult(true);
+    }
+
+    ///<inheritdoc/>
+    public Task<T?> GetAsync<T>(string key)
+    {
+        return Task.FromResult(default(T));
+    }
+
+    ///<inheritdoc/>
+    public Task<bool> SetAsync<T>(string key, T value, TimeSpan ttl)
+    {
+        return Task.FromResult(true);
+    }
+}
